a68d7306d717a95323e1ec155e2249f96b12f18b
Coder <coderdba@users.noreply.github.com>
2016-07-14 11:27:33 +0530

    Update cr-db-oracle12c-rac.sh

--- oracle-12c-kb/cr-db-oracle12c-rac.sh
+++ oracle-12c-kb/cr-db-oracle12c-rac.sh
@@ -73,6 +73,8 @@ echo "INFO - Listing DB configuration using srvctl config database -d $DBNAME_UN
 srvctl config database -d $DBNAME_UNIQUE
 
 if [$ARCHIVELOGMODE="YES"]
+then
+
 echo "INFO - Setting archivelog mode"
 
 sqlplus / as sysdba <<EOF
