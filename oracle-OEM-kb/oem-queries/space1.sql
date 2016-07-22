2812bbed4e90604b0a88bc1e0fc34712a808eada
Coder <coderdba@users.noreply.github.com>
2016-07-22 14:30:28 +0530

    Update space1.sql

--- oracle-OEM-kb/oem-queries/space1.sql
+++ oracle-OEM-kb/oem-queries/space1.sql
@@ -36,9 +36,10 @@ and metric_name = 'DATABASE_SIZE'
 and rollup_timestamp > sysdate-90
 order by 1, 2;
 
-
 prompt
 prompt  90 day old data
+prompt
+prompt NOTE - use the other one that filters by rac_database and oracle_database
 prompt
 
 select
@@ -65,6 +66,39 @@ and a.column_label = 'Used Space(GB)'
 and a.metric_name = 'DATABASE_SIZE'
 order by 1, 2;
 
+prompt
+prompt 90 day old data again
+prompt
+prompt This one filters by rac_database and oracle_database for RAC and non-rac databases
+prompt
+
+select
+a.target_name,
+a.target_type,
+a.metric_name,
+a.rollup_timestamp,
+a.average,
+a.minimum,
+a.maximum
+from
+mgmt$metric_daily a,
+(select target_name, min(rollup_timestamp) rollup_timestamp
+   from mgmt$metric_daily
+  where rollup_timestamp >= sysdate-90
+    and column_label = 'Used Space(GB)'
+    and metric_name = 'DATABASE_SIZE'
+    and (target_name like ('DB_UNIQUE_NAME_PATTERN1%') or target_name like ('DB_UNIQUE_NAME_PATTERN2%')
+    or target_name in ('DB_UNIQUE_NAME3', 'DB_UNIQUE_NAME4')
+    )
+    and (target_type = 'rac_database' or target_type = 'oracle_database')
+  group by target_name
+) b
+where
+a.target_name = b.target_name
+and a.rollup_timestamp = b.rollup_timestamp
+and a.column_label = 'Used Space(GB)'
+and a.metric_name = 'DATABASE_SIZE'
+order by 1, 2;
 
 spool off
 
