e89350e2e96b42ff31f9e00a16de10ba16ef4954
Coder <coderdba@users.noreply.github.com>
2016-07-22 14:51:04 +0530

    Update oem-db-space1.sql

--- oracle-OEM-kb/oem-queries/oem-db-space1.sql
+++ oracle-OEM-kb/oem-queries/oem-db-space1.sql
@@ -36,67 +36,42 @@ and metric_name = 'DATABASE_SIZE'
 and rollup_timestamp > sysdate-90
 order by 1, 2;
 
+-- Based on http://www.nyoug.org/Presentations/2011/March/Iotzov_OEM_Repository.pdf
 prompt
-prompt  90 day old data
+prompt 90 day old data
 prompt
-prompt NOTE - use the other one that filters by rac_database and oracle_database
-prompt
-
-select
-a.target_name,
-a.metric_name,
-a.rollup_timestamp,
-a.average,
-a.minimum,
-a.maximum
-from
-mgmt$metric_daily a,
-(select target_name, min(rollup_timestamp) rollup_timestamp
-   from mgmt$metric_daily
-  where rollup_timestamp >= sysdate-90
-    and column_label = 'Used Space(GB)'
-    and metric_name = 'DATABASE_SIZE'
-    and target_name in ('DB_UNIQUE_NAME1', 'DB_UNIQUE_NAME2')
-  group by target_name
-) b
-where
-a.target_name = b.target_name
-and a.rollup_timestamp = b.rollup_timestamp
-and a.column_label = 'Used Space(GB)'
-and a.metric_name = 'DATABASE_SIZE'
-order by 1, 2;
-
-prompt
-prompt 90 day old data again
-prompt
-prompt This one filters by rac_database and oracle_database for RAC and non-rac databases
+prompt This filters by rac_database and oracle_database for RAC and non-rac databases
+prompt so that multiple instances of a DB are not listed repeatedly
 prompt
 
 select
 a.target_name,
 a.target_type,
 a.metric_name,
+a.metric_column,
 a.rollup_timestamp,
 a.average,
 a.minimum,
 a.maximum
 from
 mgmt$metric_daily a,
-(select target_name, min(rollup_timestamp) rollup_timestamp
-   from mgmt$metric_daily
-  where rollup_timestamp >= sysdate-90
-    and column_label = 'Used Space(GB)'
-    and metric_name = 'DATABASE_SIZE'
-    and (target_name like ('DB_UNIQUE_NAME_PATTERN1%') or target_name like ('DB_UNIQUE_NAME_PATTERN2%')
-    or target_name in ('DB_UNIQUE_NAME3', 'DB_UNIQUE_NAME4')
-    )
-    and (target_type = 'rac_database' or target_type = 'oracle_database')
-  group by target_name
-) b
+(select b.target_name, b.target_guid, min(b.rollup_timestamp) rollup_timestamp
+   from mgmt$metric_daily b,
+        mgmt$target_type c
+  where b.rollup_timestamp >= sysdate-90
+    and b.column_label = 'Used Space(GB)'
+    and b.metric_name = 'DATABASE_SIZE'
+    and (b.target_name like ('DB_UNIQUE_NAME_PATTERN%') or b.target_name like ('DB_UNIQUE_NAME'))
+    and (b.target_type = 'rac_database' or (b.target_type = 'oracle_database' and c.type_qualifier3 != 'RACINST'))
+    and b.metric_guid = c.metric_guid
+    and b.target_guid = c.target_guid
+  group by b.target_name, b.target_guid
+) d
 where
-a.target_name = b.target_name
-and a.rollup_timestamp = b.rollup_timestamp
-and a.column_label = 'Used Space(GB)'
+a.target_name = d.target_name
+and a.target_guid = d.target_guid
+and a.rollup_timestamp = d.rollup_timestamp
+and a.metric_column in ('ALLOCATED_GB', 'USED_GB')
 and a.metric_name = 'DATABASE_SIZE'
 order by 1, 2;
 
