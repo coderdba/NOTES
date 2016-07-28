a86c0352d96c61e13190f95b0eb5bf67636c3b09
Coder <coderdba@users.noreply.github.com>
2016-07-28 21:18:33 +0530

    Update oem-list-db.sql

--- oracle-OEM-kb/oem-queries/oem-list-db.sql
+++ oracle-OEM-kb/oem-queries/oem-list-db.sql
@@ -12,6 +12,19 @@ where a.TARGET_TYPE='oracle_database' and a.TARGET_GUID=m.TARGET_GUID and m.PROP
 and  a.target_guid = c.target_guid
 order by 1;
 
+prompt INFO- Prod Non-RAC Databases with version
+prompt
+select a.target_name || ' NonRAC Production', c.property_value, d.property_value
+from sysman.mgmt_targets a, sysman.mgmt_target_properties m,
+     (select target_guid, property_value from sysman.mgmt_target_properties
+        where property_name='orcl_gtp_lifecycle_status' and property_value = 'PROD') c,
+     (select target_guid, property_value from sysman.mgmt_target_properties
+        where property_name='DBVersion') d
+where a.TARGET_TYPE='oracle_database' and a.TARGET_GUID=m.TARGET_GUID and m.PROPERTY_NAME='RACOption' and m.PROPERTY_VALUE='NO'
+and  a.target_guid = c.target_guid
+and  a.target_guid = d.target_guid
+order by 1;
+
 prompt INFO- Prod RAC Databases (DB UNIQUE NAMES)
 select a.target_name  || ' RAC Production'
 from sysman.mgmt_targets a, sysman.mgmt_target_properties m
