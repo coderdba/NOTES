9a48584eb15d8388373bbe2880de11161f1044b6
Coder <coderdba@users.noreply.github.com>
2016-07-29 10:19:12 +0530

    Create oem_host.sql

---
+++ oracle-OEM-kb/oem-queries/oem_host.sql
@@ -0,0 +1,13 @@
+set pages 500
+set lines 100
+
+spool host
+
+select *
+--host_name, system_config, freq, mem, disk, cpu_count, total_cpu_cores cores, physical_cpu_count phy_cpu, 
+  logical_cpu_count logical_cpu, virtual
+from   mgmt$os_hw_summary
+where  host_name = 'myhostname'
+order by 2, 1;
+
+spool off
