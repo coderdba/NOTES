4bfa6a20bb78895fe252be481376fa0d585d70a2
Coder <coderdba@users.noreply.github.com>
2016-07-29 10:29:56 +0530

    Update and rename oem_serverdetails.sql to oem_server_class_details.sql

---
+++ oracle-OEM-kb/oem-queries/oem_server_class_details.sql
@@ -0,0 +1,14 @@
+select distinct system_config from MGMT$OS_HW_SUMMARY
+  order by 1;
+
+-- Results will be something like this:
+--IBM,8202-E4B
+--PowerEdge M620
+--PowerEdge R710
+--SUNW,Netra-T12
+--SUNW,SPARC-Enterprise
+--SUNW,SPARC-Enterprise-T5220
+--UCSB-B200-M3
+--UCSC-C220-M4S
+--sun4v
+--x86_64
--- oracle-OEM-kb/oem-queries/oem_serverdetails.sql
+++
@@ -1,2 +0,0 @@
-select distinct system_config from MGMT$OS_HW_SUMMARY
-  order by 1;
