18c3c0f08b410739a12c40a672ef8f0eb215ea29
Coder <coderdba@users.noreply.github.com>
2017-03-01 22:39:22 +0530

    Create proxy-off.bat

---
+++ chef-kb/toolchain/proxy-off.bat
@@ -0,0 +1,12 @@
+@echo off
+
+:: ########################################################
+:: # Setting up Company Proxy
+:: ########################################################
+
+set HTTP_PROXY=
+set HTTPS_PROXY=
+set NO_PROXY=
+set VAGRANT_HTTP_PROXY=
+set VAGRANT_HTTPS_PROXY=
+set VAGRANT_NO_PROXY=
