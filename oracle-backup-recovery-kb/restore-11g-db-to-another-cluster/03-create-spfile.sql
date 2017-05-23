4302344c00cca95869e0593de7a34ca35c7dd1d9
Coder <coderdba@users.noreply.github.com>
2017-05-23 16:13:42 +0530

    Update 03-create-spfile.sql

--- oracle-backup-recovery-kb/restore-11g-db-to-another-cluster/03-create-spfile.sql
+++ oracle-backup-recovery-kb/restore-11g-db-to-another-cluster/03-create-spfile.sql
@@ -1,1 +1,6 @@
 create spfile from pfile='/tmp/pfile.ora';
+
+-- restart to make required control-file locations to take effect 
+-- (otherwise controlfile gets restored to dbs folder)
+shutdown;
+startup nomount;
