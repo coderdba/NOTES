8edd345f3aee949d1dd5daefa46b63896fb349d3
Coder <coderdba@users.noreply.github.com>
2017-05-26 10:36:28 +0530

    Update sqls-offloading-to-cell.sql

--- exadata-kb/scripts/sqls-offloading-to-cell.sql
+++ exadata-kb/scripts/sqls-offloading-to-cell.sql
@@ -3,3 +3,13 @@ select sql_id from v$sql where
 --or
 io_cell_offload_returned_bytes > 0
 ;
+
+select sql_id,
+    io_cell_offload_eligible_bytes qualifying,
+    io_cell_offload_eligible_bytes - io_cell_offload_returned_bytes actual,
+    round(((io_cell_offload_eligible_bytes - io_cell_offload_returned_bytes)/io_cell_offload_eligible_bytes)*100, 2) io_saved_pct,
+    sql_text
+    from v$sql
+    where io_cell_offload_returned_bytes> 0
+    and instr(sql_text, 'emp') > 0
+    and parsing_schema_name = 'BING';
