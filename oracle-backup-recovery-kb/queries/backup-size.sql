4dd1d9327c0e3eb31af47421d29e7f8778dc1f1a
Coder <coderdba@users.noreply.github.com>
2017-06-01 14:59:09 +0530

    Update backup-size.sql

--- oracle-backup-recovery-kb/queries/backup-size.sql
+++ oracle-backup-recovery-kb/queries/backup-size.sql
@@ -13,23 +13,33 @@ column db_unique_name format a15
 
 spool backup-size
 
-select distinct  
+select
         a.db_name,
         c.session_key,
         b.db_unique_name,
-        c.input_type, c.status,
-        to_char(c.start_time, 'dd/mm/yyyy hh24:mi') st_time,
-        to_char(c.end_time,'dd/mm/yyyy hh24:mi') end_time,
+        c.input_type,
+        c.status,
+        to_char(c.start_time, 'dd-mon-yyyy hh24:mi') st_time,
+        to_char(c.end_time,'dd-mon-yyyy hh24:mi') end_time,
         c.input_bytes
         --,c.time_taken_display, 3600*c.input_bytes_per_sec/1024/1024/1024 input_gbh
-from   rc_rman_status a, rc_site b, rc_rman_backup_job_details c
+--
+from   (select distinct db_name, session_key, site_key from rc_rman_status) a,
+       rc_site b,
+       rc_rman_backup_job_details c
+--
 where  b.site_key = a.site_key
   and  c.session_key = a.session_key
-  and  trunc(a.start_time) > sysdate-15
-  -- like operator to cover site1 and site2 of cl1db1 database
-  and  b.DB_UNIQUE_NAME like 'CL1DB1%'
+  and  trunc(c.start_time) > sysdate-15
+  and  b.DB_UNIQUE_NAME like 'XYZ%_SITE1'
   and  c.input_type='DB INCR'
+  -- more than 500G input
+  --and c.input_bytes/1024/1024/1024 > 500
+--
+  -- L0 like backups
+  --and c.input_bytes*0.9<c.input_bytes
 order by 3, 2, c.start_time
 ;
+
 
 spool off
