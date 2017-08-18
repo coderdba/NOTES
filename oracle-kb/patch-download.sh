a6da690a8cfb0475034119b6e4e63bee58573172
Coder <coderdba@users.noreply.github.com>
2017-08-18 14:37:20 +0530

    Create patch-download.sh

---
+++ oracle-kb/patch-download.sh
@@ -0,0 +1,3 @@
+# wget --http-user=username --http-password=password --no-check-certificate --output-document=filename "https://updates.oracle.com/Orion/Download/download_patch/filename" 
+
+wget --http-user=myuser@mycompany.com --http-password="mypassword\$123" --no-check-certificate --output-document=p25898234_12102170418ProactiveBP_Linux-x86-64_1of2.zip "https://updates.oracle.com/Orion/Services/download/p25898234_12102170418ProactiveBP_Linux-x86-64_1of2.zip?aru=21305807&patch_file=p25898234_12102170418ProactiveBP_Linux-x86-64_1of2.zip"
