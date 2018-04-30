3d5a8b328a5886990ea032b0ecce081b24dc7597
Coder <coderdba@users.noreply.github.com>
2018-04-30 11:39:38 +0530

    Update exachk_on_first_dom0.sh

--- exadata-kb/scripts-patching/exachk_on_first_dom0.sh
+++ exadata-kb/scripts-patching/exachk_on_first_dom0.sh
@@ -1,3 +1,4 @@
-exec > run_exachk_on_first_dom0.sh.out 2>> run_exachk_on_first_dom0.sh.out
+#exec > run_exachk_on_first_dom0.sh.out 2>> run_exachk_on_first_dom0.sh.out
 
+# run this in foreground as it asks for download exachk Y/N etc
 /opt/oracle.SupportTools/exachk/exachk
