5958c69aab5aebf570446d0403c2b0f8ec431929
Coder <coderdba@users.noreply.github.com>
2018-07-04 12:47:18 +0530

    Create CreateAndPopulateKeyspace.java

---
+++ cassandra-kb/cassandra-training1/java/project1/CreateAndPopulateKeyspace.java
@@ -0,0 +1,137 @@
+package com.admatic;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
+public class CreateAndPopulateKeyspace {
+    //static String[] CONTACT_POINTS = {"127.0.0.1"};
+    static String[] CONTACT_POINTS = {"172.31.92.87"};
+    static int PORT = 9042;
+
+    public static void main(String[] args) {
+
+        CreateAndPopulateKeyspace client = new CreateAndPopulateKeyspace();
+
+        try {
+
+            client.connect(CONTACT_POINTS, PORT);
+            client.createSchema();
+            client.loadData();
+            client.querySchema();
+
+        } finally {
+            client.close();
+        }
+    }
+
+    private Cluster cluster;
+
+    private Session session;
+
+    /**
+     * Initiates a connection to the cluster
+     * specified by the given contact point.
+     *
+     * @param contactPoints the contact points to use.
+     * @param port          the port to use.
+     */
+    public void connect(String[] contactPoints, int port) {
+
+        cluster = Cluster.builder()
+                .addContactPoints(contactPoints).withPort(port)
+                .build();
+
+        System.out.printf("Connected to cluster: %s%n", cluster.getMetadata().getClusterName());
+
+        session = cluster.connect();
+    }
+
+    /**
+     * Creates the schema (keyspace) and tables
+     * for this example.
+     */
+    public void createSchema() {
+
+        session.execute("CREATE KEYSPACE IF NOT EXISTS simplex WITH replication " +
+                "= {'class':'SimpleStrategy', 'replication_factor':1};");
+
+        session.execute(
+                "CREATE TABLE IF NOT EXISTS simplex.songs (" +
+                        "id uuid PRIMARY KEY," +
+                        "title text," +
+                        "album text," +
+                        "artist text," +
+                        "tags set<text>," +
+                        "data blob" +
+                        ");");
+
+        session.execute(
+                "CREATE TABLE IF NOT EXISTS simplex.playlists (" +
+                        "id uuid," +
+                        "title text," +
+                        "album text, " +
+                        "artist text," +
+                        "song_id uuid," +
+                        "PRIMARY KEY (id, title, album, artist)" +
+                        ");");
+    }
+
+    /**
+     * Inserts data into the tables.
+     */
+    public void loadData() {
+
+        session.execute(
+                "INSERT INTO simplex.songs (id, title, album, artist, tags) " +
+                        "VALUES (" +
+                        "756716f7-2e54-4715-9f00-91dcbea6cf50," +
+                        "'La Petite Tonkinoise'," +
+                        "'Bye Bye Blackbird'," +
+                        "'Joséphine Baker'," +
+                        "{'jazz', '2013'})" +
+                        ";");
+
+        session.execute(
+                "INSERT INTO simplex.playlists (id, song_id, title, album, artist) " +
+                        "VALUES (" +
+                        "2cc9ccb7-6221-4ccb-8387-f22b6a1b354d," +
+                        "756716f7-2e54-4715-9f00-91dcbea6cf50," +
+                        "'La Petite Tonkinoise'," +
+                        "'Bye Bye Blackbird'," +
+                        "'Joséphine Baker'" +
+                        ");");
+    }
+
+    /**
+     * Queries and displays data.
+     */
+    public void querySchema() {
+
+        ResultSet results = session.execute(
+                "SELECT * FROM simplex.playlists " +
+                        "WHERE id = 2cc9ccb7-6221-4ccb-8387-f22b6a1b354d;");
+
+        System.out.printf("%-30s\t%-20s\t%-20s%n", "title", "album", "artist");
+        System.out.println("-------------------------------+-----------------------+--------------------");
+
+        for (Row row : results) {
+
+            System.out.printf("%-30s\t%-20s\t%-20s%n",
+                    row.getString("title"),
+                    row.getString("album"),
+                    row.getString("artist"));
+
+        }
+
+    }
+
+    /**
+     * Closes the session and the cluster.
+     */
+    public void close() {
+        session.close();
+        cluster.close();
+    }
+}
