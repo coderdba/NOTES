1387a743bae39daf759fd7cb27695c070b1b739a
Coder <coderdba@users.noreply.github.com>
2018-07-04 12:46:16 +0530

    Create App.java

---
+++ cassandra-kb/cassandra-training1/java/src/App.java
@@ -0,0 +1,13 @@
+package com.admatic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
