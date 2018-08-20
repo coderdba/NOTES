acc9a61923664e766efed6a52484baeac2194a4e
Coder <coderdba@users.noreply.github.com>
2018-08-20 22:27:23 +0530

    Update create_db_12c_non_rac_fs.sh

--- oracle-12c-kb/create_db_scripts/create_db_12c_non_rac_fs.sh
+++ oracle-12c-kb/create_db_scripts/create_db_12c_non_rac_fs.sh
@@ -1,5 +1,7 @@
 # CREATE 12C NON-RAC DB ON FILESYSTEM
 
+# NOTE: sga_target=2064M is the minimum SGA setting dbca takes
+
 export DBNAME=ORANR2
 export SID=ORANR2
 export DBNAME_UNIQUE=ORANR2_SITE1
@@ -10,4 +12,4 @@ export LD_LIBRARY_PATH=$ORACLE_HOME/lib
 
 export PATH=/u01/app/oracle/product/12.1.0/dbhome_1/perl/bin:/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/oracle/.local/bin:/home/oracle/bin:/u01/app/oracle/product/12.1.0/dbhome_1/bin
 
-/u01/app/oracle/product/12.1.0/dbhome_1/bin/dbca -silent -createDatabase -gdbName $DBNAME_UNIQUE -sid $SID -databaseType MULTIPURPOSE -characterSet AL32UTF8 -nationalCharacterSet AL16UTF16 -initParams db_name=$DBNAME,db_unique_name=$DBNAME_UNIQUE,db_block_size=8192,sga_target=2048M -templatename General_Purpose.dbc -sysPassword Oracle_123 -systemPassword Oracle_123 -createAsContainerDatabase true -numberOfPdbs 1 -pdbName $PDBNAME -pdbAdminUserName PDBADMIN -pdbAdminPassword Oracle_123 -storageType FS -datafileDestination /oradata/data -recoveryAreaDestination /oradata/fra -redoLogFileSize 100 -emConfiguration NONE -sampleSchema false
+/u01/app/oracle/product/12.1.0/dbhome_1/bin/dbca -silent -createDatabase -gdbName $DBNAME_UNIQUE -sid $SID -databaseType MULTIPURPOSE -characterSet AL32UTF8 -nationalCharacterSet AL16UTF16 -initParams db_name=$DBNAME,db_unique_name=$DBNAME_UNIQUE,db_block_size=8192,sga_target=2064M -templatename General_Purpose.dbc -sysPassword Oracle_123 -systemPassword Oracle_123 -createAsContainerDatabase true -numberOfPdbs 1 -pdbName $PDBNAME -pdbAdminUserName PDBADMIN -pdbAdminPassword Oracle_123 -storageType FS -datafileDestination /oradata/data -recoveryAreaDestination /oradata/fra -redoLogFileSize 100 -emConfiguration NONE -sampleSchema false
