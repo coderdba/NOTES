c67731a67dbf831c6209a901518dfec205c330fa
Coder <coderdba@users.noreply.github.com>
2018-11-28 18:27:24 +0530

    Update flannel-install-node.sh

--- kubernetes-kb/scripts/vbox-oel/flannel-install-node.sh
+++ kubernetes-kb/scripts/vbox-oel/flannel-install-node.sh
@@ -1,7 +1,19 @@
+echo INFO - First edit /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+echo  and add --node-ip directive
+
+echo
+echo SLEEPING 100 SECONDS FOR YOU TO EDIT THE FILE
+sleep 100
+
 mkdir -p /root/.kube
 scp k00:/root/.kube/config /root/.kube/config
 
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 cat /proc/sys/net/bridge/bridge-nf-call-iptables
+
+systemctl enable kubelet
+systemctl enable docker
+systemctl daemon-reload
+service docker start
 
 kubectl apply -f /root/kubeinstall/kube-flannel.yml
