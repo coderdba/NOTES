68e6422c71aeb2aa8c685e5d8a0d1d9142de2cd4
Coder <coderdba@users.noreply.github.com>
2018-11-28 15:33:54 +0530

    Update flannel-install.sh

--- kubernetes-kb/scripts/vbox-oel/flannel-install.sh
+++ kubernetes-kb/scripts/vbox-oel/flannel-install.sh
@@ -1,7 +1,7 @@
 mkdir -p /root/.kube
 cp /etc/kubernetes/admin.conf /root/.kube/config
 
-echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 cat /proc/sys/net/bridge/bridge-nf-call-iptables
 
 kubectl apply -f /root/kubeinstall/kube-flannel.yml
