66672a7214a834a1901dde40aebd742d58df2672
Coder <coderdba@users.noreply.github.com>
2018-11-28 18:28:28 +0530

    Create node-join.sh

---
+++ kubernetes-kb/scripts/vbox-oel/node-join.sh
@@ -0,0 +1,19 @@
+echo INFO - First edit /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+echo  and add --node-ip directive
+
+echo
+echo SLEEPING 100 SECONDS FOR YOU TO EDIT THE FILE
+sleep 100
+
+mkdir -p /root/.kube
+scp k00:/root/.kube/config /root/.kube/config
+
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+cat /proc/sys/net/bridge/bridge-nf-call-iptables
+
+systemctl enable kubelet
+systemctl enable docker
+systemctl daemon-reload
+service docker start
+
+kubeadm join 192.168.11.100:6443 --token s8gwke.yxate9aj24yygbhu --discovery-token-ca-cert-hash sha256:ea6d4afd5ae0e4a002d6a1c609dd121f4756e01390d30103b91591e1bf9c6519
