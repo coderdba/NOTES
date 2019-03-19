c10166e02b8197d2ed76543e3ca24b6a429aeaaf
Coder <coderdba@users.noreply.github.com>
2019-03-19 11:40:42 +0530

    Create create_cert_and_key1.sh

---
+++ certificates-kb/create_cert_and_key1.sh
@@ -0,0 +1,18 @@
+# IN THIS CSR IS NOT GENERATED SEPARATELY
+# https://deliciousbrains.com/ssl-certificate-authority-for-local-https-development/
+
+#- GENERATE KEY
+# NOTE: This asks for a passphrase
+
+openssl genrsa -des3 -out myCA.key 2048
+
+#- GENERATE CERT
+# NOTE: This asks for the passphrase entered earlier in key generation
+
+openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem
+
+#- REMOVE PASSPHRASE FROM KEY
+# https://serverfault.com/questions/543385/restarting-nginx-keeps-asking-pem-pass-phrase
+# NOTE: Provide the same passphrase entered earlier in key generation
+
+openssl rsa -in myCA.key.withPassPhrase -out myCA.key
