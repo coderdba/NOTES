5b78ce87c42c01a9d5b97dc788a7548761faee1a
Coder <coderdba@users.noreply.github.com>
2019-03-19 11:26:38 +0530

    Create create_cert_and_key.sh

---
+++ certificates-kb/create_cert_and_key.sh
@@ -0,0 +1,17 @@
+# Create key
+echo
+echo INFO - creating key
+echo
+openssl genrsa -des3 -out awx.key 1024
+
+# Create csr
+echo
+echo INFO - creating csr
+echo
+openssl req -new -key awx.key -out awx.csr
+
+# Create cert
+echo
+echo INFO - creating crt
+echo
+openssl x509 -req -days 365 -in awx.csr -signkey awx.key -out awx.crt
