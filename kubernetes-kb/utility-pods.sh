8995a090f279c496b6859f5015f4330a31f16de4
Coder <coderdba@users.noreply.github.com>
2019-06-14 11:46:24 +0530

    Update utility-pods.sh

--- kubernetes-kb/utility-pods.sh
+++ kubernetes-kb/utility-pods.sh
@@ -3,3 +3,10 @@ https://medium.com/@pczarkowski/kubernetes-tip-run-an-interactive-pod-d701766a12
 kubectl run -i --tty --rm debug --image=busybox --restart=Never -- sh
 
 LINUX CURL
+docker pull pstauffer/curl
+
+docker run -d --name curl pstauffer/curl:latest
+docker run --rm --name curl pstauffer/curl:latest curl --version
+docker run --rm --name curl pstauffer/curl:latest curl http://www.google.ch
+
+kubectl run -i --tty --rm debug --image=pstauffer/curl --restart=Never -- sh
