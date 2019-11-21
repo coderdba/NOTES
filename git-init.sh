53f12b3e3aa8a170126d5bc80e893db9457c4159
Coder <coderdba@users.noreply.github.com>
2019-11-21 15:15:27 +0530

    Update git-init.sh

--- git-init.sh
+++ git-init.sh
@@ -3,5 +3,5 @@ git init
 git add README.md
 git add .
 git commit -m "first commit"
-git remote add origin git@github.com:gowrishdba/NOTES.git
+git remote add origin git@github.com:coderdba/NOTES.git
 git push -u origin master
