d78bda85fe599c247d103fa87936ffcc3f488f57
Coder <coderdba@users.noreply.github.com>
2019-11-21 19:46:17 +0530

    Create kube-installs.sh

---
+++ kubernetes-kb/kub-machines/1.15.5/kube-installs.sh
@@ -0,0 +1,18 @@
+  
+echo INFO - installing kubelet
+yum install -y kubelet-1.15.5-0
+
+echo INFO - installing kubectl
+yum install -y kubectl-1.15.5-0
+
+echo INFO - installing kubeadm
+yum install -y kubeadm-1.15.5-0
+
+echo
+echo
+echo verifying
+echo
+rpm -qa |grep kube
+which kubelet
+which kubectl
+which kubeadm
