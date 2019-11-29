7e320e71cddf720d633f774d510a64ed503b8576
Coder <coderdba@users.noreply.github.com>
2019-11-29 13:11:58 +0530

    Create derive1-run.sh

---
+++ packer-kb/samples/centos7-gm1/derive1-run.sh
@@ -0,0 +1,23 @@
+echo Starting `date`
+echo Setting environment
+
+export VERSION=0.0.1
+#export PACKER_LOG_PATH=/dev/stderr 
+export PACKER_LOG_PATH=/dev/stdout 
+export PACKER_LOG=1 
+export PACKER_CACHE_DIR=/data/packer_cache 
+
+echo Starting packer command
+
+/data/bin/packer build derive1-packer.json
+#/data/bin/packer build -machine-readable derive1-packer.json
+
+echo Packer command completed
+
+echo Moving new image to /data/images
+/bin/mv img/centos7 /data/images/centos7-gm-derive1-${VERSION}.qcow2
+
+echo Removing the temporary folder img
+rmdir img
+
+echo Ending `date`
