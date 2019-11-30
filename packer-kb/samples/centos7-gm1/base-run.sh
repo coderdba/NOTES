8eb62650792688bbb7b784095c2667f09cab5552
Coder <coderdba@users.noreply.github.com>
2019-11-30 16:47:34 +0530

    Update base-run.sh

--- packer-kb/samples/centos7-gm1/base-run.sh
+++ packer-kb/samples/centos7-gm1/base-run.sh
@@ -5,19 +5,19 @@ export VERSION=0.0.1
 #export PACKER_LOG_PATH=/dev/stderr 
 export PACKER_LOG_PATH=/dev/stdout 
 export PACKER_LOG=1 
-export PACKER_CACHE_DIR=/data/packer_cache 
+export PACKER_CACHE_DIR=/var/lib/packer-work/packer_cache 
 
 echo Starting packer command
 
 #/data/bin/packer build base-packer.json
 #/data/bin/packer build -machine-readable base-packer.json
 
-/data/bin/packer build -var 'rpm=true' base-packer.json
+/root/packer/packer build -var 'rpm=true' base-packer.json
 
 echo Packer command completed
 
 echo Moving new image to /data/images
-/bin/mv img/centos7 /data/images/centos7-gm-${VERSION}.qcow2
+/bin/mv img/centos7 /var/lib/packer-work/images/centos7-gm1-base-${VERSION}.qcow2
 
 echo Removing the temporary folder img
 rmdir img
