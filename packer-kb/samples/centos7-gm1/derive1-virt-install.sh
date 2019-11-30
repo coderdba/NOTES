6b979c2d69f17e5ad9cf3438709ac0d833bad8a1
Coder <coderdba@users.noreply.github.com>
2019-11-30 18:06:09 +0530

    Create derive1-virt-install.sh

---
+++ packer-kb/samples/centos7-gm1/derive1-virt-install.sh
@@ -0,0 +1,36 @@
+root_disk_path=/var/lib/vm/gm1-root.qcow2
+image_file=/var/lib/packer-work/images/centos7-gm1-derive1-0.0.1.qcow2
+
+cp $image_file $root_disk_path
+
+# virt-install \
+#   --name=$vm_name \
+#   --ram=$MEM \
+#   --vcpus=$CPU \
+#   --disk path=${root_disk_path},format=qcow2 \
+#   --disk path=${docker_disk_path},format=qcow2 \
+#   --disk path=${etcd_disk_path},format=qcow2 \
+#   --disk path=${certs_disk_path},format=qcow2 \
+#   --disk path=${inject_files_path},format=RAW \
+#   --disk path=${shared_disk_path},format=RAW \
+#   --disk path=${local_persistent_disk_path},format=qcow2 \
+#   --disk path=${seed_path},device=cdrom \
+#   --network bridge=br30,model=virtio \
+#   --network bridge=br30,model=virtio \
+#   --os-type=linux \
+#   --os-variant rhel7 \
+#   --nographics \
+#   --import
+
+virt-install \
+  --name=gm1 \
+  --ram=1024 \
+  --vcpus=2 \
+  --disk path=/var/lib/vm/gm1-root.qcow2,format=qcow2 \
+  --disk path=seed-virt-install.iso,device=cdrom \
+  --network bridge=br0,model=virtio \
+  --network bridge=br0,model=virtio \
+  --os-type=linux \
+  --os-variant rhel7 \
+  --nographics \
+  --import
