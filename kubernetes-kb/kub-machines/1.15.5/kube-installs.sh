08f4cf50e73dbee20e6be8fb4bafca11a08c50c1
Coder <coderdba@users.noreply.github.com>
2019-12-15 11:59:46 +0530

    Update kube-installs.sh

--- kubernetes-kb/kub-machines/1.15.5/kube-installs.sh
+++ kubernetes-kb/kub-machines/1.15.5/kube-installs.sh
@@ -8,6 +8,9 @@ yum install -y kubectl-1.15.5-0
 echo INFO - installing kubeadm
 yum install -y kubeadm-1.15.5-0
 
+echo INFO - installing kubernetes-cni
+yum install kubernetes-cni-0.7.5-0
+
 echo
 echo
 echo verifying
