9765893c1c1f7d1176659be8866bd8a4171f046c
Coder <coderdba@users.noreply.github.com>
2019-12-15 11:03:33 +0530

    Update image-pulls.sh

--- kubernetes-kb/kub-machines/1.12.8/image-pulls.sh
+++ kubernetes-kb/kub-machines/1.12.8/image-pulls.sh
@@ -1,3 +1,6 @@
+docker pull quay.io/coreos/etcd:v3.3.10
+docker pull quay.io/coreos/etcd:v3.2.24
+
 docker pull gcr.io/google_containers/kube-apiserver-amd64:v1.12.8
 docker pull gcr.io/google_containers/kube-scheduler-amd64:v1.12.8 
 docker pull gcr.io/google_containers/kube-controller-manager-amd64:v1.12.8
@@ -8,7 +11,7 @@ docker pull k8s.gcr.io/k8s-dns-kube-dns:1.14.13
 docker pull k8s.gcr.io/k8s-dns-dnsmasq-nanny:1.14.13
 docker pull k8s.gcr.io/k8s-dns-sidecar:1.14.13
 
-# being used in store with kubernetes 1.12.8
+# being used with kubernetes 1.12.8
 docker pull quay.io/coreos/flannel:v0.9.0-amd64 
 
 # latest as in kube-flannel.yml 
