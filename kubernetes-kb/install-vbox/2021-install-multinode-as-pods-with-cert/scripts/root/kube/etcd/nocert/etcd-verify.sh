d5b95922bbafff50170ac6eef684df22fbebea17
Coder <coderdba@users.noreply.github.com>
2021-08-14 11:34:16 +0530

    Delete etcd-verify.sh

--- kubernetes-kb/install-vbox/2021-install-multinode-as-pods-with-cert/scripts/root/kube/etcd/nocert/etcd-verify.sh
+++
@@ -1,8 +0,0 @@
-export NODE1=192.168.99.101
-
-docker ps -a  | grep etcd
-
-etcdctl --endpoints=http://${NODE1}:2379 member list
-etcdctl --endpoints=http://${192.168.99.101}:2379 member list
-
-docker exec etcd /bin/sh -c "export ETCDCTL_API=3 && /usr/local/bin/etcdctl member list"
