aee27f2b6a15ccaa642d1190d44b7f95441f7ad3
Coder <coderdba@users.noreply.github.com>
2022-03-30 15:07:23 +0530

    Create authProviderReactAadMsal.js

---
+++ azure-kb/react-sso/react-aad-msal/authProviderReactAadMsal.js
@@ -0,0 +1,37 @@
+// authProvider.js
+import { MsalAuthProvider, LoginType } from 'react-aad-msal';
+
+// Msal Configurations
+const config = {
+  auth: {
+    authority: 'https://login.microsoftonline.com/0473845-fa1a-42e7-9cf1-eb416c396f2d',  --> THE APP REGISTRATION PIECE
+    clientId: '453c58ff-1854-41fb-b2e3-05779f34d581/.default',  --> APPLICATION ID IN AZURE
+    postLogoutRedirectUri: window.location.origin,
+    redirectUri: window.location.origin,
+    validateAuthority: true,
+    // After being redirected to the "redirectUri" page, should user
+    // be redirected back to the Url where their login originated from?
+    navigateToLoginRequestUrl: true
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: true
+  }
+};
+
+// Authentication Parameters
+const authenticationParameters = {
+  scopes: [
+    '453c58ff-1854-41fb-b2e3-05779f34d581/.default'  --> APPLICATION ID IN AZURE
+    //'user.read',
+    //'https://05d75c05-fa1a-42e7-9cf1-eb416c396f2d.onmicrosoft.com/564c69ff-2965-41fb-b2e3-05779f34d581/.default>'
+  ]
+}
+
+// Options
+const options = {
+  loginType: LoginType.Popup,
+  tokenRefreshUri: window.location.origin + '/auth.html'
+}
+
+export const authProvider = new MsalAuthProvider(config, authenticationParameters, options)
