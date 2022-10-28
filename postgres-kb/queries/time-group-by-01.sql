4259a27cda7a4d8dcc5f9bf22a8059e9aff776a1
Coder <coderdba@users.noreply.github.com>
2022-10-28 14:28:11 +0530

    Create time-group-by-01.sql

---
+++ postgres-kb/queries/time-group-by-01.sql
@@ -0,0 +1,27 @@
+-- https://gis.stackexchange.com/questions/57582/group-by-timestamp-interval-10-minutes-postgresql
+-- 10 minute grouping based on timestamp column in a table
+-- group by that period
+-- get sum, count, avg etc
+
+
+select count(*),
+to_timestamp(floor((extract('epoch' from date_time) / 600 )) * 600)
+AT TIME ZONE 'UTC' as interval_alias
+FROM ai1.metric_table0 GROUP BY interval_alias;
+
+select count(*), avg(metric_value),
+to_timestamp(floor((extract('epoch' from date_time) / 600 )) * 600)
+AT TIME ZONE 'UTC' as interval_alias
+FROM ai1.metric_table0 GROUP BY interval_alias;
+
+/**** THE TABLE
+mydb=> \d ai1.metric_table0
+                        Table "ai1.metric_table0"
+    Column    |           Type           | Collation | Nullable | Default
+--------------+--------------------------+-----------+----------+---------
+ date_time    | timestamp with time zone |           | not null |
+ metric_name  | character varying(50)    |           | not null |
+ metric_value | double precision         |           | not null |
+Indexes:
+    "metric_table0_pk" PRIMARY KEY, btree (date_time, metric_name)
+******/
