c48f2802e23e65e206f6b20cf105841ae3f63d77
Coder <coderdba@users.noreply.github.com>
2022-10-31 09:50:13 +0530

    Update correlation.sql

--- postgres-kb/queries/correlation.sql
+++ postgres-kb/queries/correlation.sql
@@ -2,3 +2,9 @@
 
 select corr(height,weight) 
 from people;
+
+-- This may not work, but is a placeholder to try something like this
+select corr(a.metric_value, b.metric_value) 
+from 
+(select metric_value from ai1.metric_table0 where metric_name = 'latency1') a,
+(select metric_value from ai1.metric_table0 where metric_name = 'cpu') b;
