c3f6dab3c42a4772b6ff1c9f04586f5a39d340fc
Coder <coderdba@users.noreply.github.com>
2022-12-13 08:19:19 +0530

    Create InvalidInputException.java

---
+++ spring-kb/postgres-jpa-model/exceptions/InvalidInputException.java
@@ -0,0 +1,8 @@
+package myco.com.sbpgcrudjpa1.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+	
+   public InvalidInputException(String message) {
+	   super(message);
+   }
+}
