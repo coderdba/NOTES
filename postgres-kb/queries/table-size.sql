19abbb4659eb045bea6eeae87fee31142227c6f9
Coder <coderdba@users.noreply.github.com>
2022-12-15 11:22:19 +0530

    Create table-size.sql

---
+++ postgres-kb/queries/table-size.sql
@@ -0,0 +1,18 @@
+SELECT pg_size_pretty(pg_total_relation_size('sp.date1'))
+from sp.date1;
+
+select schemaname as table_schema,
+       relname as table_name,
+       pg_size_pretty(pg_relation_size(relid)) as data_size
+from pg_catalog.pg_stat_all_tables
+order by pg_relation_size(relid) desc;
+
+select schemaname as table_schema,
+       relname as table_name,
+       pg_size_pretty(pg_relation_size(relid)) as data_size
+from pg_catalog.pg_statio_user_tables
+order by pg_relation_size(relid) desc;
+
+SELECT pg_size_pretty(pg_total_relation_size('sp.date1'))
+from sp.date1;
+
