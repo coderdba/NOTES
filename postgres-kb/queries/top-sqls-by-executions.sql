9c2304771b2a7794f925de4fafbddec705b380a7
Coder <coderdba@users.noreply.github.com>
2022-12-15 12:06:20 +0530

    Update top-sqls-by-executions.sql

--- postgres-kb/queries/top-sqls-by-executions.sql
+++ postgres-kb/queries/top-sqls-by-executions.sql
@@ -1,3 +1,5 @@
+Version 13:
+
 SELECT
 d.datname as db, 
 r.rolname AS username, 
@@ -20,4 +22,24 @@ FROM pg_stat_statements,   pg_roles r,   pg_database d
 WHERE r.oid = userid  
 AND   calls > 0 
 AND d.oid = dbid 
+ORDER BY calls DESC limit 20
+
+Version 11:
+
+SELECT 
+d.datname as DbName,
+r.rolname AS UserName,
+Round((100 * total_time / Sum(total_time::numeric) OVER ())::numeric, 2) AS CpuPercent,
+coalesce(calls, 0) AS Calls,Round(total_time::numeric, 2) AS TotalTime,
+Round(mean_time::numeric, 2) AS MeanTime,
+coalesce(shared_blks_hit, 0) AS SharedBlksHit,
+coalesce(shared_blks_read, 0) AS SharedBlksRead,
+coalesce(shared_blks_read, 0) AS SharedBlksWritten,
+coalesce(local_blks_hit, 0) AS LocalBlksHit,
+coalesce(local_blks_read, 0) AS LocalBlksRead,
+coalesce(local_blks_read, 0) AS LocalBlksWritten,
+Substring(query, 1, 800)     AS PartialQuery
+FROM pg_stat_statements,pg_roles r,pg_database d
+WHERE r.oid = useridAND d.oid = dbid
+AND calls > 0
 ORDER BY calls DESC limit 20
