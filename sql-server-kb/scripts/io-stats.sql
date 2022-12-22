4fe20a2f447808c699b5cf4a0ed5ba55b42e5c78
Coder <coderdba@users.noreply.github.com>
2022-12-22 11:52:55 +0530

    Create io-stats.sql

---
+++ sql-server-kb/scripts/io-stats.sql
@@ -0,0 +1,46 @@
+-- https://www.sqlshack.com/sql-server-troubleshooting-disk-i-o-problems/
+
+
+-- I/O Stalls
+-- I/O stall time is an indicator that can be used to detect I/O problems. The dm_io_virtual_file_stats is a dynamic management function that gives detailed information about the stall times of the data and log files so it will simplify the SQL Server troubleshooting process. This dynamic management function takes two parameters first one is database id, and the second one is the database file number.
+
+SELECT * FROM
+sys.dm_io_virtual_file_stats (   
+    { database_id | NULL },  
+    { file_id | NULL }  
+)  
+
+-- We can execute this dynamic management function like the below for all databases.
+
+select Db.name ,vfs.* from
+  sys.dm_io_virtual_file_stats(NULL, NULL) AS VFS
+    JOIN sys.databases AS Db 
+  ON vfs.database_id = Db.database_id
+  
+  
+--> Info from the site
+
+-- Misconfigured or malfunctioning disk subsystems
+-- Insufficient disk performances
+-- Applications that generate redundant I/O activities
+-- Poor designed or unoptimized queries
+-- Analyzing the symptoms should be a major principle to clarify the underlying reason that causes the I/O issues on SQL Server. Otherwise, we can waste time dealing with irrelevant issues or discussing the issues with system or storage administrators unnecessarily. Wait types give very useful information for SQL Server troubleshooting. The following wait types can indicate I/O problems, but these wait types do not suffice to decide any problem on the disks.
+
+-- PAGEIOLATCH_*
+-- WRITELOG
+-- ASYNC_IO_COMPLETION
+-- At first, we will briefly describe these wait types and their relations to the I/O problems.
+
+-- PAGEIOLATCH_*
+-- SQL Server reserves an area on the memory to itself, and this area uses to cache data and index pages to reduce the disk activities. This reserved memory area is called Buffer Pool. The working mechanism of the buffer pool is very simple; the data loads from the disk to the memory when any request has been received for reading or changing, and they process in the buffer pool. The data is written to the disk again when it is modified. In light of this information, PAGEIOLATCH_* occurs when transferring data from disk to buffer pool. It is very normal to detect some PAGEIOLATCH_* however, it indicates a problem when we see this wait type frequently and more than the other wait types. PAGEIOLATCH_* does not indicate disk problems by oneself because this wait type can occur for a variety of reasons. For example:
+
+-- Outdated statistics or poorly designed indexes can cause to PAGEIOLATCH_* waits because these types of problems cause redundant disk activities
+-- Enabling CDC (Change Data Capture) option can cause extra I/O workload
+-- Insufficient memory can cause PAGEIOLATCH_* problems because SQL Server does not keep the data pages long enough in the buffer cache. The other sign of this problem is the Page Life Expectancy metric
+-- WRITELOG
+-- When any modification is performed in the database, SQL Server writes this modification to log buffer, and then it writes this buffer data to disk. Therefore, this wait type is related to the physical disk that contains the log file (ldf). Placing log files (ldf) on as fast and dedicated disks as possible will be the right approach to overcome these problems. At the same time, performance statistics of physical disks that store ldf files should be considered when this problem occurs. The log data is written into the disk sequentially, and the reading process is also performed sequentially. Due to this working principle, the disks selected for the log files must perform well for the sequential read and write throughput along with the minimum latency.
+
+-- ASYNC_IO_COMPLETION
+-- This wait type occurs when the SQL Server processes backup and restore operations; however, when this operation takes more time than usual, it might be a warning for the I/O problems. The BACKUPIO can be seen with the ASYNC_IO_COMPLETION so we can consider about any disk problem.
+
+
