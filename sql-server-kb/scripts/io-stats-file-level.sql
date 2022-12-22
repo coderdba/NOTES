194721468ccfffbddcf296f23b20f1b8c1410391
Coder <coderdba@users.noreply.github.com>
2022-12-22 12:27:21 +0530

    Update and rename io-stats.sql to io-stats-file-level.sql

--- sql-server-kb/scripts/io-stats-file-level.sql
+++ sql-server-kb/scripts/io-stats-file-level.sql
@@ -18,7 +18,24 @@ select Db.name ,vfs.* from
   ON vfs.database_id = Db.database_id
   
   
---> Info from the site
+-- Combined query
+SELECT  DB_NAME(vfs.database_id) AS database_name ,physical_name AS [Physical Name],
+        size_on_disk_bytes / 1024 / 1024. AS [Size of Disk] ,
+        CAST(io_stall_read_ms/(1.0 + num_of_reads) AS NUMERIC(10,1)) AS [Average Read latency] ,
+        CAST(io_stall_write_ms/(1.0 + num_of_writes) AS NUMERIC(10,1)) AS [Average Write latency] ,
+        CAST((io_stall_read_ms + io_stall_write_ms)
+/(1.0 + num_of_reads + num_of_writes) 
+AS NUMERIC(10,1)) AS [Average Total Latency],
+        num_of_bytes_read / NULLIF(num_of_reads, 0) AS    [Average Bytes Per Read],
+        num_of_bytes_written / NULLIF(num_of_writes, 0) AS   [Average Bytes Per Write]
+FROM    sys.dm_io_virtual_file_stats(NULL, NULL) AS vfs
+  JOIN sys.master_files AS mf 
+    ON vfs.database_id = mf.database_id AND vfs.file_id = mf.file_id
+ORDER BY [Average Total Latency] DESC
+
+===================================
+Info from the site
+===================================
 
 -- Misconfigured or malfunctioning disk subsystems
 -- Insufficient disk performances
