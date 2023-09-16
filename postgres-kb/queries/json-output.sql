544c9386168a8f8c4f860a6604d1ec07fd667962
Coder <coderdba@users.noreply.github.com>
2023-09-16 11:38:32 +0530

    Update json-output.sql

--- postgres-kb/queries/json-output.sql
+++ postgres-kb/queries/json-output.sql
@@ -17,6 +17,7 @@ select to_jsonb(id) from schema1.products;
 select to_jsonb(name) from schema1.products;
 
 row_to_json --> each row is presented as a json. however, all of them are not made an array or nested json.
+Note: Each row's json is again enclosed in double quotes
 select row_to_json(*) from schema1.products; --> Does not work
 select row_to_json(t) from  (select * from schema1.products) t; --> works
 "{""id"":1,""name"":""again something new"",""price"":9992323.98}"
@@ -24,14 +25,14 @@ select row_to_json(t) from  (select * from schema1.products) t; --> works
 "{""id"":20,""name"":""new1"",""price"":1.22}"
 
 array_agg(row_to_json(t)) --> array of jsons of individual rows in the form  ({"{}","{}","{}"})
-Note: Array is enclosed in {}. Each element is a "{}" - note the double quotes. Inside "{}" it will be name:value pairs of jsons.
+Note: results in an array or sql-array enclosed in {}. Each element is a "{}" - note the double quotes. Inside "{}" it will be name:value pairs of jsons.
       Essentially, each row's json is treated as an array element of string type
 select array_agg(row_to_json(t)) from  (select * from schema1.products) t; 
 {"{\"id\":1,\"name\":\"again something new\",\"price\":9992323.98}","{\"id\":3,\"name\":\"again something new 2\",\"price\":2222323.22}","{\"id\":20,\"name\":\"new1\",\"price\":1.22}"}
 {"{"id":1,"name":"again something new","price":9992323.98}","{"id":3,"name":"again something new 2","price":2222323.22}","{"id":20,"name":"new1","price":1.22}"}
 
 array_to_json(array_agg(row_to_json(t))) --> json of jsons of individual rows in the form [{},{}]
-Note: A Json is enclosed in [].  Each element in it is a {} without double quotes. json of each row is treated as an element of an outer json.
+Note: results in a json array enclosed in [].  Each element in it is a {} without double quotes. json of each row is treated as an element of an outer json.
 select array_to_json(array_agg(row_to_json(t))) from  (select * from schema1.products) t; 
 [{"id":1,"name":"again something new","price":9992323.98},{"id":3,"name":"again something new 2","price":2222323.22},{"id":20,"name":"new1","price":1.22}]
 
