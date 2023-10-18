b88aef81b842156c2dc67b49562051f050e408c8
Coder <coderdba@users.noreply.github.com>
2023-10-18 16:22:27 +0530

    Update influx-metric-ora-using-sqlplus.sh

--- oracle-kb/scripts/shell-scripts/influx-metric-ora-using-sqlplus.sh
+++ oracle-kb/scripts/shell-scripts/influx-metric-ora-using-sqlplus.sh
@@ -17,21 +17,39 @@ set head off
 set feed off
 set echo off
 
-connect xsremon/$SREMON_PASSWORD@192.168.29.197:1521/XEPDB1
+connect sremon/$SREMON_PASSWORD@192.168.29.197:1521/XEPDB1
 --select sysdate from dual;
 --select (cast (systimestamp at time zone 'UTC' as date) - date '1970-01-01') * 86400 from dual;
 
 select 
 'autotron_oradb_up'||','||
-'instance_name=' || a.instance_name || ' ' || 1 || ' ' || 
+'instance_name=' || instance_name || ' ' || 1 || ' ' || 
 (cast (systimestamp at time zone 'UTC' as date) - date '1970-01-01') * 86400 
-from v\$instance a; 
+from v\$instance; 
+
+select 
+'autotron_oradb_account_status'||','||
+'username=' || username || ',' ||
+'account_status=' || account_status || ' ' || 1 || ' ' ||
+(cast (systimestamp at time zone 'UTC' as date) - date '1970-01-01') * 86400 
+from dba_users
+where username = 'SREMON' or username like 'AUTOTRON%';
 
 EOF
 
 #if (grep ORA $outfile)
 if (grep ORA $outfile > /dev/null 2>> /dev/null)
 then
+
+echo autotron_oradb_up,instance_name=xe 0 $current_unix_epoch_sec
+echo autotron_oradb_account_status,username=error 1 $current_unix_epoch_sec
+
+else
+
+cat $outfile
+
+fi
+
 
 echo autotron_oradb_up,instance_name=xe 0 $current_unix_epoch_sec
 
