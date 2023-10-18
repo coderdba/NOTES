40d8d9f69c186e22d1d28c26b4283d6bd7ab9b5e
Coder <coderdba@users.noreply.github.com>
2023-10-18 17:42:54 +0530

    Update influx-metric-ora-using-sqlplus.sh

--- oracle-kb/scripts/shell-scripts/influx-metric-ora-using-sqlplus.sh
+++ oracle-kb/scripts/shell-scripts/influx-metric-ora-using-sqlplus.sh
@@ -1,4 +1,12 @@
 
+#
+#
+#
+#
+#
+#
+#
+
 export PATH=$PATH:/root/oracle/instantclient_21_11
 export LD_LIBRARY_PATH=/root/oracle/instantclient_21_11
 
@@ -6,6 +14,7 @@ this_script=$0
 this_script_basename=`basename $this_script`
 outfile=/tmp/${this_script_basename}.out
 current_unix_epoch_sec=`date '+%s'`
+#current_unix_epoch_ms=`date '+%s'`
 
 # Influx format
 # myMeasurement,tag1=value1,tag2=value2 fieldKey="fieldValue" 1556813561098000000
@@ -16,43 +25,57 @@ sqlplus -s /nolog <<EOF > $outfile 2>> $outfile
 set head off
 set feed off
 set echo off
+set lines 200
 
-connect sremon/$SREMON_PASSWORD@192.168.29.197:1521/XEPDB1
+connect monuser/$MONUSER_PASSWORD@192.168.29.197:1521/XEPDB1
+
 --select sysdate from dual;
 --select (cast (systimestamp at time zone 'UTC' as date) - date '1970-01-01') * 86400 from dual;
 
+--select 
+--'autotron_oradb'||','||
+--'instance_name=' || instance_name || ' up=' || 1 || ' ' || 
+--(cast (systimestamp at time zone 'UTC' as date) - date '1970-01-01') * 86400000
+--from v\$instance; 
+
+--select 
+--'autotron_oradb'||','||
+--'username=' || username || ',' ||
+--'account_status=' || account_status || ' account_status_found=' || 1 || ',' ||
+----'expiry_date_epoch_sec=' || (cast (expiry_date at time zone 'UTC' as date) - date '1970-01-01') * 86400 || ' ' ||
+--(cast (systimestamp at time zone 'UTC' as date) - date '1970-01-01') * 86400000
+--from dba_users
+--where username = 'SREMON' or username like 'AUTOTRON%';
+
+
 select 
-'autotron_oradb_up'||','||
-'instance_name=' || instance_name || ' ' || 1 || ' ' || 
-(cast (systimestamp at time zone 'UTC' as date) - date '1970-01-01') * 86400 
+'autotron_oradb'||','||
+'instance_name=' || instance_name || ' up=' || 1 
 from v\$instance; 
 
 select 
-'autotron_oradb_account_status'||','||
+'autotron_oradb'||','||
 'username=' || username || ',' ||
-'account_status=' || account_status || ' ' || 1 || ' ' ||
-(cast (systimestamp at time zone 'UTC' as date) - date '1970-01-01') * 86400 
+'account_status=' || account_status || ' account_status_found=' || 1 
+|| ',' ||
+'expiry_date_epoch_sec=' || (expiry_date - date '1970-01-01') * 86400 
 from dba_users
 where username = 'SREMON' or username like 'AUTOTRON%';
 
+
 EOF
 
-#if (grep ORA $outfile)
-if (grep ORA $outfile > /dev/null 2>> /dev/null)
+if (grep ORA- $outfile > /dev/null 2>> /dev/null)
 then
 
-echo autotron_oradb_up,instance_name=xe 0 $current_unix_epoch_sec
-echo autotron_oradb_account_status,username=error 1 $current_unix_epoch_sec
+  #echo autotron_oradb,instance_name=xe up=0 $current_unix_epoch_sec
+  #echo autotron_oradb,username=collection_error account_status_found=1 $current_unix_epoch_sec
+
+  echo autotron_oradb,instance_name=xe up=0
+  echo autotron_oradb,username=collection_error account_status_found=1 
 
 else
 
-cat $outfile
+  cat $outfile
 
 fi
-
-
-echo autotron_oradb_up,instance_name=xe 0 $current_unix_epoch_sec
-
-fi
-
-
