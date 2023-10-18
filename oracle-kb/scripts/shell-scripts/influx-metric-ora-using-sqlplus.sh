b7e6c1b2904d3324f3b2db2fa724e137cb2b6cce
Coder <coderdba@users.noreply.github.com>
2023-10-18 15:38:03 +0530

    Update influx-metric-ora-using-sqlplus.sh

--- oracle-kb/scripts/shell-scripts/influx-metric-ora-using-sqlplus.sh
+++ oracle-kb/scripts/shell-scripts/influx-metric-ora-using-sqlplus.sh
@@ -1,20 +1,25 @@
+
 export PATH=$PATH:/root/oracle/instantclient_21_11
 export LD_LIBRARY_PATH=/root/oracle/instantclient_21_11
+
+this_script=$0
+this_script_basename=`basename $this_script`
+outfile=/tmp/${this_script_basename}.out
+current_unix_epoch_sec=`date '+%s'`
 
 # Influx format
 # myMeasurement,tag1=value1,tag2=value2 fieldKey="fieldValue" 1556813561098000000
 
-sqlplus -s /nolog <<EOF
+
+sqlplus -s /nolog <<EOF > $outfile 2>> $outfile
 
 set head off
 set feed off
 set echo off
 
-connect sremon/$SREMON_PASSWORD@192.168.29.197:1521/XEPDB1
-
-select sysdate from dual;
-
-select (cast (systimestamp at time zone 'UTC' as date) - date '1970-01-01') * 86400 from dual;
+connect xsremon/$SREMON_PASSWORD@192.168.29.197:1521/XEPDB1
+--select sysdate from dual;
+--select (cast (systimestamp at time zone 'UTC' as date) - date '1970-01-01') * 86400 from dual;
 
 select 
 'autotron_oradb_up'||','||
@@ -23,4 +28,13 @@ select
 from v\$instance a; 
 
 EOF
+
+#if (grep ORA $outfile)
+if (grep ORA $outfile > /dev/null 2>> /dev/null)
+then
+
+echo autotron_oradb_up,instance_name=xe 0 $current_unix_epoch_sec
+
+fi
+
 
