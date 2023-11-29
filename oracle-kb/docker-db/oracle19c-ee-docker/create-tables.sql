39813f0c9c959a78749320a35801ae1634203334
Coder <coderdba@users.noreply.github.com>
2023-11-29 16:19:00 +0530

    Update create-tables.sql

--- oracle-kb/docker-db/oracle19c-ee-docker/create-tables.sql
+++ oracle-kb/docker-db/oracle19c-ee-docker/create-tables.sql
@@ -21,6 +21,7 @@ create search index srchidx on person_collection_t (jdoc) for json
 
 */
 
+---------------------------------
 CREATE TABLE tenants (
   ID                       NUMBER        GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1),
   NAME                     VARCHAR2(250) NOT NULL, 
@@ -29,6 +30,7 @@ CREATE TABLE tenants (
   CONSTRAINT               tenants_name_uk UNIQUE (NAME)
 );
 
+---------------------------------
 CREATE TABLE targets (
         ID                  NUMBER GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
         NAME                VARCHAR2(50) NOT NULL,
@@ -44,15 +46,13 @@ CREATE TABLE targets (
 -- NOTE: PK and UK will already have indexes - no need to create them separately
 
 -- Search Index which can do search all over the json
-create search index targets_target_attributes_json_searchidx on targets (TARGET_ATTRIBUTES) for json;
+create search index targets_json_search_idx1 on targets (TARGET_ATTRIBUTES) for json;
 
 -- To search individual json fields
 -- NOTE: This is not working yet
-create index targets_target_attributes_app on targets ( TARGET_ATTRIBUTES.cluster.app.string() );
+create index targets_json_idx1 on targets ( TARGET_ATTRIBUTES.wlcluster.app.string() );
 
-create index custid on mytab ( jtext.customer.id.number() )
-create index custname on mytab ( jtext.customer.name.string() )
-
+--------------------------------- 
 CREATE TABLE tenant_targets (
     ID                 NUMBER GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
     NAME               VARCHAR2(50) NOT NULL,
@@ -64,6 +64,7 @@ CREATE TABLE tenant_targets (
     CONSTRAINT tenant_and_target_uk UNIQUE (TENANT_ID, NAME)
 );
 
+---------------------------------
 -- HELPFUL QUERIES TO COMBINE DATA
 select a.name, b.name, c.name 
 from
@@ -78,14 +79,70 @@ select *
 from targets t
 where json_textcontains(t.TARGET_ATTRIBUTES, '$.cluster.processes', 'vm1');
 
+-- Using on a json field
+select * from targets where json_value(target_attributes, '$.wlcluster.app') = 'app1';
+
 -- Querying on an array in the json
 -- NOTE: THESE ARE NOT HELPFUL IN OUR DATA FOR TARGETS YET
 select *
 from   t1
 where  json_exists(json_data, '$.words.pages?(@.number() == 40)');
 
-select *
+select *  
 from   targets t
 where  json_exists(t.TARGET_ATTRIBUTES, '$.cluster.processes.node?(@.string() == 'vm1')'));
 
+------------------------------------------------------
+EXPLAIN PLANS
 
+- with only json search index
+SQL>  select * from targets where json_value(target_attributes, '$.wlcluster.app') = 'app1';
+
+Execution Plan
+----------------------------------------------------------
+Plan hash value: 255216017
+
+--------------------------------------------------------------------------------------------------------
+| Id  | Operation                   | Name                     | Rows  | Bytes | Cost (%CPU)| Time     |
+--------------------------------------------------------------------------------------------------------
+|   0 | SELECT STATEMENT            |                          |     1 |  1073 |     4   (0)| 00:00:01 |
+|*  1 |  TABLE ACCESS BY INDEX ROWID| TARGETS                  |     1 |  1073 |     4   (0)| 00:00:01 |
+|*  2 |   DOMAIN INDEX              | TARGETS_JSON_SEARCH_IDX1 |       |       |     4   (0)| 00:00:01 |
+--------------------------------------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+   1 - filter(JSON_VALUE("TARGET_ATTRIBUTES" FORMAT JSON , '$.wlcluster.app' RETURNING
+              VARCHAR2(4000) NULL ON ERROR)='app1')
+   2 - access("CTXSYS"."CONTAINS"("TARGETS"."TARGET_ATTRIBUTES",'{app1} INPATH
+              (/wlcluster/app)')>0)
+
+Note
+-----
+   - dynamic statistics used: dynamic sampling (level=2)
+
+-- with json field specific index added
+SQL>  select * from targets where json_value(target_attributes, '$.wlcluster.app') = 'app1';
+
+Execution Plan
+----------------------------------------------------------
+Plan hash value: 662454342
+
+---------------------------------------------------------------------------------------------------------
+| Id  | Operation                           | Name              | Rows  | Bytes | Cost (%CPU)| Time     |
+---------------------------------------------------------------------------------------------------------
+|   0 | SELECT STATEMENT                    |                   |     1 |  3063 |     2   (0)| 00:00:01 |
+|   1 |  TABLE ACCESS BY INDEX ROWID BATCHED| TARGETS           |     1 |  3063 |     2   (0)| 00:00:01 |
+|*  2 |   INDEX RANGE SCAN                  | TARGETS_JSON_IDX1 |     1 |       |     1   (0)| 00:00:01 |
+---------------------------------------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+   2 - access(JSON_VALUE("TARGET_ATTRIBUTES" FORMAT JSON , '$.wlcluster.app.string()' RETURNING
+              VARCHAR2(4000) NULL ON ERROR)='app1')
+
+Note
+-----
+   - dynamic statistics used: dynamic sampling (level=2)
