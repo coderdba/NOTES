2b3666490fdc494726832d242a6550b2360a16ac
Coder <coderdba@users.noreply.github.com>
2024-08-13 14:12:54 +0530

    Create top-sql-by-exec-time-mean.sql

---
+++ sql-server-kb/scripts/top-sql-by-exec-time-mean.sql
@@ -0,0 +1,13 @@
+    SELECT TOP(15) qs.execution_count AS [Execution Count], (qs.total_physical_reads)/1000.0 AS [Total Physical Reads in ms], 
+      (qs.total_physical_reads/qs.execution_count)/1000.0 AS [Avg Physical Reads in ms], 
+      (qs.total_logical_reads)/1000.0 AS [Total Logical Reads in ms], 
+      (qs.total_logical_reads/qs.execution_count)/1000.0 AS [Avg Logical Reads in ms], 
+      (qs.total_worker_time)/1000.0 AS [Total Worker Time in ms], 
+      (qs.total_worker_time/qs.execution_count)/1000.0 AS [Avg Worker Time in ms], 
+      (qs.total_elapsed_time)/1000.0 AS [Total Elapsed Time in ms], 
+      (qs.total_elapsed_time/qs.execution_count)/1000.0 AS [Avg Elapsed Time in ms], 
+      qs.creation_time AS [Creation Time] , LEFT(t.text, 1000) AS [Partial Query Text] 
+      FROM sys.dm_exec_query_stats AS qs WITH (NOLOCK) 
+      CROSS APPLY sys.dm_exec_sql_text(plan_handle) AS t 
+      CROSS APPLY sys.dm_exec_query_plan(plan_handle) AS qp 
+      WHERE t.dbid = DB_ID() ORDER BY [Avg Elapsed Time in ms] DESC OPTION (RECOMPILE);
