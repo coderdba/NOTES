9c791ab62e5ac6a7b77721d2e6d3747f7af5d363
Coder <coderdba@users.noreply.github.com>
2024-08-14 10:14:30 +0530

    Update top-stored-proc-by-exec-time-mean.sql

--- sql-server-kb/scripts/top-stored-proc-by-exec-time-mean.sql
+++ sql-server-kb/scripts/top-stored-proc-by-exec-time-mean.sql
@@ -1,7 +1,10 @@
 -- https://learn.microsoft.com/en-us/sql/relational-databases/system-dynamic-management-views/sys-dm-exec-procedure-stats-transact-sql?view=sql-server-ver16
 
-SELECT TOP 20 d.object_id, 
-    d.database_id, OBJECT_NAME(object_id, database_id) 'Stored Proc Name',   
+-- For specific database
+SELECT TOP 20 
+    -- d.object_id, 
+    db_name(d.database_id) 'DB Name', 
+	OBJECT_NAME(object_id, database_id) 'Stored Proc Name',   
     --d.sql_handle,
 	d.total_elapsed_time/1000000 as [total_elapsed_time_s],  
     d.total_elapsed_time/1000000/d.execution_count AS [avg_elapsed_time_s],  
@@ -9,13 +12,17 @@ SELECT TOP 20 d.object_id,
 	d.execution_count,
 	d.cached_time, 
 	d.last_execution_time
-FROM sys.dm_exec_procedure_stats AS d  
+FROM sys.dm_exec_procedure_stats AS d 
+where db_name(d.database_id) = 'MyDB'
 ORDER BY [avg_elapsed_time_s] DESC;
+--ORDER BY [total_worker_time] DESC;
 
+-- Across all databases
 SELECT TOP 10 d.object_id, d.database_id, OBJECT_NAME(object_id, database_id) 'proc name',   
     d.sql_handle,
     d.cached_time, d.last_execution_time, d.total_elapsed_time,  
     d.total_elapsed_time/d.execution_count AS [avg_elapsed_time],  
     d.last_elapsed_time, d.execution_count  
 FROM sys.dm_exec_procedure_stats AS d  
-ORDER BY [total_worker_time] DESC;
+ORDER BY [avg_elapsed_time_s] DESC;
+--ORDER BY [total_worker_time] DESC;
