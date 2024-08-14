67d20d105e5724e0f858640aff6a764ff7d5633e
Coder <coderdba@users.noreply.github.com>
2024-08-14 09:25:48 +0530

    Update top-stored-proc-by-exec-time-mean.sql

--- sql-server-kb/scripts/top-stored-proc-by-exec-time-mean.sql
+++ sql-server-kb/scripts/top-stored-proc-by-exec-time-mean.sql
@@ -1,6 +1,7 @@
 -- https://learn.microsoft.com/en-us/sql/relational-databases/system-dynamic-management-views/sys-dm-exec-procedure-stats-transact-sql?view=sql-server-ver16
 
 SELECT TOP 10 d.object_id, d.database_id, OBJECT_NAME(object_id, database_id) 'proc name',   
+    d.sql_handle,
     d.cached_time, d.last_execution_time, d.total_elapsed_time,  
     d.total_elapsed_time/d.execution_count AS [avg_elapsed_time],  
     d.last_elapsed_time, d.execution_count  
