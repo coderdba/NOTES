3c128d336f2208f2229f1ea21f93c80cd3316847
Coder <coderdba@users.noreply.github.com>
2024-08-14 09:28:19 +0530

    Create row-count-by-query.sql

---
+++ sql-server-kb/scripts/row-count-by-query.sql
@@ -0,0 +1,17 @@
+-- https://learn.microsoft.com/en-us/sql/relational-databases/system-dynamic-management-views/sys-dm-exec-query-stats-transact-sql?view=sql-server-ver16#examples
+-- The following example returns row count aggregate information (total rows, minimum rows, maximum rows and last rows) for queries
+
+SELECT qs.execution_count,  
+    SUBSTRING(qt.text,qs.statement_start_offset/2 +1,   
+                 (CASE WHEN qs.statement_end_offset = -1   
+                       THEN LEN(CONVERT(nvarchar(max), qt.text)) * 2   
+                       ELSE qs.statement_end_offset end -  
+                            qs.statement_start_offset  
+                 )/2  
+             ) AS query_text,   
+     qt.dbid, dbname= DB_NAME (qt.dbid), qt.objectid,   
+     qs.total_rows, qs.last_rows, qs.min_rows, qs.max_rows  
+FROM sys.dm_exec_query_stats AS qs   
+CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) AS qt   
+WHERE qt.text like '%SELECT%'   
+ORDER BY qs.execution_count DESC;
