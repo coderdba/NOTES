b1e71efb666208e6eff47d0971f1b2ef98c44852
Coder <coderdba@users.noreply.github.com>
2024-08-16 13:35:25 +0530

    Update index-columns.sql

--- sql-server-kb/scripts/index-columns.sql
+++ sql-server-kb/scripts/index-columns.sql
@@ -2,6 +2,8 @@
 
 USE mydb;
 GO
+
+-- FOR SPECIFIC TABLE
 SELECT i.name AS index_name
     ,COL_NAME(ic.object_id,ic.column_id) AS column_name
     ,ic.index_column_id
@@ -11,3 +13,32 @@ FROM sys.indexes AS i
 INNER JOIN sys.index_columns AS ic
     ON i.object_id = ic.object_id AND i.index_id = ic.index_id
 WHERE i.object_id = OBJECT_ID('myTable');
+
+-- FOR ALL TABLES
+-- https://stackoverflow.com/questions/765867/list-of-all-index-index-columns-in-sql-server-db
+SELECT 
+     TableName = t.name,
+     IndexName = ind.name,
+     IndexId = ind.index_id,
+     ColumnId = ic.index_column_id,
+     ColumnName = col.name,
+     ind.*,
+     ic.*,
+     col.* 
+FROM 
+     sys.indexes ind 
+INNER JOIN 
+     sys.index_columns ic ON  ind.object_id = ic.object_id and ind.index_id = ic.index_id 
+INNER JOIN 
+     sys.columns col ON ic.object_id = col.object_id and ic.column_id = col.column_id 
+INNER JOIN 
+     sys.tables t ON ind.object_id = t.object_id 
+WHERE 
+    -- UNCOMMENT THESE TO EXCLUDE PRIMARY AND UNIQUE KEYS
+     --ind.is_primary_key = 0 
+     --AND ind.is_unique = 0 
+     --AND ind.is_unique_constraint = 0 
+     --AND 
+      t.is_ms_shipped = 0 
+ORDER BY 
+     t.name, ind.name, ind.index_id, ic.is_included_column, ic.key_ordinal;
