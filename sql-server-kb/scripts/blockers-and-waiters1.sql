fdd107bf66f1500624e69c2ab79c741ffcd7f8fe
Coder <coderdba@users.noreply.github.com>
2024-08-27 17:52:09 +0530

    Create blockers-and-waiters1.sql

---
+++ sql-server-kb/scripts/blockers-and-waiters1.sql
@@ -0,0 +1,65 @@
+
+-------------------------------------------------------
+-- https://learn.microsoft.com/en-us/troubleshoot/sql/database-engine/performance/understand-resolve-blocking
+-- BLOCKERS AND WAITERS
+
+WITH cteBL (session_id, blocking_these) AS 
+(SELECT s.session_id, blocking_these = x.blocking_these FROM sys.dm_exec_sessions s 
+CROSS APPLY    (SELECT isnull(convert(varchar(6), er.session_id),'') + ', '  
+                FROM sys.dm_exec_requests as er
+                WHERE er.blocking_session_id = isnull(s.session_id ,0)
+                AND er.blocking_session_id <> 0
+                FOR XML PATH('') ) AS x (blocking_these)
+)
+SELECT s.session_id, blocked_by = r.blocking_session_id, bl.blocking_these
+, batch_text = t.text, input_buffer = ib.event_info, * 
+FROM sys.dm_exec_sessions s 
+LEFT OUTER JOIN sys.dm_exec_requests r on r.session_id = s.session_id
+INNER JOIN cteBL as bl on s.session_id = bl.session_id
+OUTER APPLY sys.dm_exec_sql_text (r.sql_handle) t
+OUTER APPLY sys.dm_exec_input_buffer(s.session_id, NULL) AS ib
+WHERE blocking_these is not null or r.blocking_session_id > 0
+ORDER BY len(bl.blocking_these) desc, r.blocking_session_id desc, r.session_id;
+
+-------------------------------------------------------
+-- https://stackoverflow.com/questions/4365970/finding-blocking-locking-queries-in-ms-sql-mssql
+
+-- just session info
+select top 2 * from sys.dm_exec_sessions where database_id = db_id();
+
+-- queries blocked
+SELECT * 
+  FROM sys.dm_exec_requests
+  WHERE DB_NAME(database_id) = 'YourDBName' 
+    AND blocking_session_id <> 0
+To get the query itself use this one:
+
+SELECT text,* 
+  FROM sys.dm_exec_requests
+  CROSS APPLY sys.dm_exec_sql_text(sql_handle)
+  WHERE DB_NAME(database_id) = 'YourDBName' 
+    AND blocking_session_id <> 0
+
+-- locked table and query causing the issue.
+SELECT  L.request_session_id AS SPID, 
+        DB_NAME(L.resource_database_id) AS DatabaseName,
+        O.Name AS LockedObjectName, 
+        P.object_id AS LockedObjectId, 
+        L.resource_type AS LockedResource, 
+        L.request_mode AS LockType,
+        ST.text AS SqlStatementText,        
+        ES.login_name AS LoginName,
+        ES.host_name AS HostName,
+        TST.is_user_transaction as IsUserTransaction,
+        AT.name as TransactionName,
+        CN.auth_scheme as AuthenticationMethod
+FROM    sys.dm_tran_locks L
+        JOIN sys.partitions P ON P.hobt_id = L.resource_associated_entity_id
+        JOIN sys.objects O ON O.object_id = P.object_id
+        JOIN sys.dm_exec_sessions ES ON ES.session_id = L.request_session_id
+        JOIN sys.dm_tran_session_transactions TST ON ES.session_id = TST.session_id
+        JOIN sys.dm_tran_active_transactions AT ON TST.transaction_id = AT.transaction_id
+        JOIN sys.dm_exec_connections CN ON CN.session_id = ES.session_id
+        CROSS APPLY sys.dm_exec_sql_text(CN.most_recent_sql_handle) AS ST
+WHERE   resource_database_id = db_id()
+ORDER BY L.request_session_id
