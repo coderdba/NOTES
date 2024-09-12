685924885eaf5b6f8c55f1a85f3fe04f31c6f4dc
Coder <coderdba@users.noreply.github.com>
2024-09-12 13:39:49 +0530

    Create 01-perf-views.sql

---
+++ sql-server-kb/scripts/01-perf-views.sql
@@ -0,0 +1,11 @@
+sys.dm_exec_connections
+  https://www.mssqltips.com/sqlservertip/5502/return-sql-server-connections-information-using-sysdmexecconnections/
+  
+sys.dm_exec_input_buffer to retrieve the last T-SQL command.
+  https://www.mssqltips.com/sqlservertip/5122/retrieve-actively-running-tsql-statements-from-sql-server/
+  
+sys.dm_exec_sessions.
+  https://www.mssqltips.com/sqlservertip/5122/retrieve-actively-running-tsql-statements-from-sql-server/
+
+sys.dm_exec_sessions
+  https://www.mssqltips.com/sqlservertip/5507/understanding-and-using-sysdmexecsessions-in-sql-server/
