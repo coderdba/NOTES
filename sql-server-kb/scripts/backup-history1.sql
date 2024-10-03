c8f4d0430c7ae854d7fc9e021ddf979001d065e4
Coder <coderdba@users.noreply.github.com>
2024-10-03 21:04:07 +0530

    Update backup-history1.sql

--- sql-server-kb/scripts/backup-history1.sql
+++ sql-server-kb/scripts/backup-history1.sql
@@ -37,9 +37,9 @@ ORDER BY
    msdb.dbo.backupset.backup_start_date
 
 -- WITH BACKUP MEDIA INFO (from the website)
---------------------------------------------------------------------------------- 
--- Database Backups for all databases For Previous Week 
---------------------------------------------------------------------------------- 
+------------------------------------------------------------------------------------------------------
+-- Database Backups for all databases For Previous Week - WITH MEDIA PHYSICAL-DRIVE INFORMATION
+------------------------------------------------------------------------------------------------------
 SELECT 
    CONVERT(CHAR(100), SERVERPROPERTY('Servername')) AS Server, 
    msdb.dbo.backupset.database_name, 
@@ -65,3 +65,30 @@ WHERE
 ORDER BY 
    msdb.dbo.backupset.database_name, 
    msdb.dbo.backupset.backup_finish_date 
+
+-- same query with distinct of only the media
+SELECT DISTINCT physical_device_name 
+FROM (
+    SELECT 
+        CONVERT(CHAR(100), SERVERPROPERTY('Servername')) AS Server, 
+        msdb.dbo.backupset.database_name, 
+        msdb.dbo.backupset.backup_start_date, 
+        msdb.dbo.backupset.backup_finish_date, 
+        msdb.dbo.backupset.expiration_date, 
+        CASE msdb.dbo.backupset.type 
+            WHEN 'D' THEN 'Full' 
+            WHEN 'I' THEN 'Differential'
+            WHEN 'L' THEN 'Log'
+        END AS backup_type, 
+        FORMAT(msdb.dbo.backupset.backup_size, 'N0') AS backup_size,
+        msdb.dbo.backupmediafamily.logical_device_name, 
+        msdb.dbo.backupmediafamily.physical_device_name, 
+        msdb.dbo.backupset.name AS backupset_name, 
+        msdb.dbo.backupset.description 
+    FROM 
+        msdb.dbo.backupmediafamily 
+        INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id 
+    WHERE 
+        CONVERT(DATETIME, msdb.dbo.backupset.backup_start_date, 102) >= GETDATE() - 1
+) AS subquery;
+
