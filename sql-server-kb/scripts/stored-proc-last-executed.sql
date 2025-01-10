1f0465ac92199d4b15416e2289e5bc870a88c109
Coder <coderdba@users.noreply.github.com>
2025-01-10 11:47:32 +0530

    Update stored-proc-last-executed.sql

--- sql-server-kb/scripts/stored-proc-last-executed.sql
+++ sql-server-kb/scripts/stored-proc-last-executed.sql
@@ -1,10 +1,24 @@
+-- LAST EXECUTED TIME
+SELECT sc.name AS schema_name,
+       p.name AS sp_name,
+       max(st.last_execution_time)
+FROM sys.procedures AS p
+INNER JOIN sys.schemas AS sc
+    ON p.[schema_id] = sc.[schema_id]
+LEFT OUTER JOIN sys.dm_exec_procedure_stats AS st
+    ON p.[object_id] = st.[object_id]
+WHERE st.last_execution_time IS NOT NULL
+GROUP BY sc.name, p.name
+ORDER BY sc.name ASC, p.name ASC;
 
+-- EACH EXECUTION TIME AND COUNT
 SELECT sc.name as schema_name, p.name as sp_name, st.last_execution_time, st.execution_count
 FROM sys.procedures AS p 
 INNER JOIN sys.schemas AS sc
 ON p.[schema_id] = sc.[schema_id]LEFT OUTER JOIN sys.dm_exec_procedure_stats AS st ON p.[object_id] = st.[object_id] 
 ORDER BY sc.name asc, st.last_execution_time desc, p.name asc
 
+-- ALL STORED PROCS
 select distinct sc.name as schema_name, p.name as sp_name
 from sys.procedures p,
      sys.schemas sc,
