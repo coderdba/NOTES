5ec5669727439546a248497312aae84be609ac14
Coder <coderdba@users.noreply.github.com>
2025-02-24 18:02:21 +0530

    Create db-top-stored-proc-by-phy-reads-mean.sql

---
+++ sql-server-kb/forensics-2025/db-top-stored-proc-by-phy-reads-mean.sql
@@ -0,0 +1,17 @@
+SELECT TOP 20 
+    -- d.object_id, 
+    db_name(d.database_id) 'DB Name', 
+	OBJECT_NAME(object_id, database_id) 'Stored Proc Name',   
+    --d.sql_handle,
+    d.total_physical_reads/d.execution_count AS 'Avg Phy Reads',  
+  d.max_physical_reads as 'Max Phy Reads', 
+  d.min_physical_reads as 'Min Phy Reads', 
+  d.last_physical_reads as 'Last Phy Reads', 
+	d.total_physical_reads as 'Tot Phy Reads',  
+	d.total_logical_writes as 'Tot Logical Writes',
+	d.execution_count as 'Exec Count',
+	d.cached_time as 'Cached Time', 
+	d.last_execution_time as 'Last Exec Time'
+FROM sys.dm_exec_procedure_stats AS d 
+where db_name(d.database_id) = 'WebEc'
+ORDER BY 'Avg Phy Reads' DESC;
